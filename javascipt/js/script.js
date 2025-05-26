b376456393c41aae08f77999f420190da48797f4
jinsol <goqk923@gmail.com>
2025-05-26 17:19:52 +0900

    수정한 내용

---
+++ javascipt/js/script.js
@@ -0,0 +1,28 @@
+let heading = document.querySelector('#heading');
+heading.onclick=function(){
+        heading.style.color='red'
+        }
+
+// alert("안녕하세요")
+// confirm("오늘 수업 쭉 자바스크립트")
+// prompt("집에 가고싶다","네")
+
+// let name=prompt("당신의 이름을 입력하세요");
+// console.log(name)
+let width;
+let height;
+
+width=200;
+height=50;
+
+let area=width*height
+console.log(area)
+let totalArea=5;
+
+const pi=3.14;
+let radius=prompt("반지름값을 입력해주세요")
+
+let area1=pi*radius*radius;
+console.log(area1)
+
+
